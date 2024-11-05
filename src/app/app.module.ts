dbdf9e89aeb7881dc427a309e57938a8e009021f
CEF2003 <charbeleliasfarhat@gmail.com>
2024-11-05 21:30:13 +0200

    Working with modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { TaskComponent } from "./tasks/task/task.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/card/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
+
+@NgModule({
+    declarations: [AppComponent, HeaderComponent, UserComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, FormsModule, SharedModule, TasksModule]
+})
+export class AppModule {
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 // type User = {
 //   id: string;
@@ -10,10 +9,9 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
+  standalone: false,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
-  imports: [CardComponent]
+  styleUrl: './user.component.css'
 })
 export class UserComponent {
 
